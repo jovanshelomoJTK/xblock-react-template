2fceaebf0cae5989882e0de4019a0da02f846e7a
Jovan Shelomo <jovanshelomo@gmail.com>
2024-02-24 20:28:38 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,15 @@ function App() {
       <Button variant="secondary" onClick={() => setCount2((c) => c + 1)}>
         Click to increment: {count2}
       </Button>
+      <p className="font-bold">Warna yang sudah di custom di template ini:</p>
+      <p className="text-primary">primary</p>
+      <p className="text-secondary">secondary</p>
+      <p className="text-foreground">foreground</p>
+      <p className="text-muted">muted</p>
+      <p className="text-success">success</p>
+      <p className="text-destructive">destructive</p>
+      <p className="text-warning">warning</p>
+      <p className="text-container">container</p>
     </div>
   );
 }
